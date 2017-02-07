6cc81f38a8512f927582dba98e7bd54c559d0d76
Gonçalo Cabrita <_@gmcabrita.com>
2017-02-07 20:41:36 +0000

    Move txn_buffer behavior to the log_sender_vnode.

--- src/inter_dc_log_sender_vnode.erl
+++ src/inter_dc_log_sender_vnode.erl
@@ -31,35 +31,42 @@
 
 %% API
 -export([
-	 send/2,
-	 update_last_log_id/2,
-	 start_timer/1,
-	 send_stable_time/2]).
+    send/2,
+    update_last_log_id/2,
+    start_timer/1,
+    send_stable_time/2]).
 
 %% VNode methods
 -export([
-  init/1,
-  start_vnode/1,
-  handle_command/3,
-  handle_coverage/4,
-  handle_exit/3,
-  handoff_starting/2,
-  handoff_cancelled/1,
-  handoff_finished/2,
-  handle_handoff_command/3,
-  handle_handoff_data/2,
-  encode_handoff_item/2,
-  is_empty/1,
-  terminate/2,
-  delete/1]).
+    init/1,
+    start_vnode/1,
+    handle_command/3,
+    handle_coverage/4,
+    handle_exit/3,
+    handoff_starting/2,
+    handoff_cancelled/1,
+    handoff_finished/2,
+    handle_handoff_command/3,
+    handle_handoff_data/2,
+    encode_handoff_item/2,
+    is_empty/1,
+    terminate/2,
+    delete/1]).
 
 %% Vnode state
 -record(state, {
-  partition :: partition_id(),
-  buffer, %% log_tx_assembler:state
-  last_log_id :: #op_number{},
-  last_log_id_buffered :: #op_number{},
-  timer :: any()
+    partition :: partition_id(),
+    buffer, %% log_tx_assembler:state
+    last_log_id :: #op_number{}, % last log id that was broadcasted
+    timer :: any(), % when this timer ends a heartbeat is propagated
+    %% The record fields below are used for collecting transactions for a small duration of time.
+    %% Once the buffer timer runs out it compacts the collection of transactions and
+    %% then broadcasts it to remote DCs.
+    %% The txn_buffer is then wiped clean and the buffer_timer restarted.
+    %% Note: these are only used if ?BUFFER_TXNS is true.
+    txn_buffer :: [#interdc_txn{}], % collection of completed transactions
+    last_log_id_buffered :: #op_number{}, % last log id that was buffered
+    buffer_timer :: any() % when this timer
 }).
 
 %%%% API --------------------------------------------------------------------+
@@ -90,47 +97,64 @@ send_stable_time(Partition, Time) ->
 start_vnode(I) -> riak_core_vnode_master:get_vnode_pid(I, ?MODULE).
 
 init([Partition]) ->
-  {ok, #state{
-    partition = Partition,
-    buffer = log_txn_assembler:new_state(),
-    last_log_id = #op_number{},
-    last_log_id_buffered = #op_number{},
-    timer = none
-  }}.
+    {ok, set_buffer_timer(#state{
+        partition = Partition,
+        buffer = log_txn_assembler:new_state(),
+        last_log_id = #op_number{},
+        timer = none,
+        last_log_id_buffered = #op_number{},
+        txn_buffer = [],
+        buffer_timer = none
+    })}.
 
 %% Start the timer
 handle_command({start_timer}, _Sender, State) ->
-    {reply, ok, set_timer(true, State)};
+    {reply, ok, set_heartbeat_timer(true, State)};
 
 handle_command({update_last_log_id, OpId}, _Sender, State = #state{partition = Partition}) ->
     lager:debug("Updating last log id at partition ~w to: ~w", [Partition, OpId]),
-    {reply, ok, State#state{last_log_id = OpId}};
+    {reply, ok, State#state{last_log_id = OpId, last_log_id_buffered = OpId}};
 
 %% Handle the new operation
 %% -spec handle_command({log_event, #log_record{}}, pid(), #state{}) -> {noreply, #state{}}.
 handle_command({log_event, LogRecord}, _Sender, State) ->
-  %% Use the txn_assembler to check if the complete transaction was collected.
-  {Result, NewBufState} = log_txn_assembler:process(LogRecord, State#state.buffer),
-  State1 = State#state{buffer = NewBufState},
-  State2 = case Result of
-    %% If the transaction was collected
-    {ok, Ops} ->
-      Txn = inter_dc_txn:from_ops(Ops, State1#state.partition, State#state.last_log_id_buffered),
-      case ?BUFFER_TXNS of
-        true -> buffer(State1, Txn);
-        false -> broadcast(State1, Txn)
-      end;
-    %% If the transaction is not yet complete
-    none -> State1
-  end,
-  {noreply, State2};
+    %% Use the txn_assembler to check if the complete transaction was collected.
+    {Result, NewBufState} = log_txn_assembler:process(LogRecord, State#state.buffer),
+    State1 = State#state{buffer = NewBufState},
+    State2 = case Result of
+        %% If the transaction was collected
+        {ok, Ops} ->
+            case ?BUFFER_TXNS of
+                true ->
+                    Txn = inter_dc_txn:from_ops(Ops, State1#state.partition, State#state.last_log_id),
+                    broadcast(State1, Txn);
+                false ->
+                    Txn = inter_dc_txn:from_ops(Ops, State1#state.partition, State#state.last_log_id_buffered),
+                    buffer(State1, Txn)
+            end
+        %% If the transaction is not yet complete
+        none -> State1
+    end,
+    {noreply, State2};
+
+handle_command(txn_send, _Sender, State = #state{partition = Partition, txn_buffer = Buffer}) ->
+    OpId = case Buffer of
+        [] -> State#state.last_log_id;
+        _ ->
+            Buf = lists:reverse(Buffer),
+            spawn(fun() -> inter_dc_txn_buffer:compact_and_broadcast(Buf) end),
+            inter_dc_txn:last_log_opid(hd(Buffer))
+    end,
+    State1 = set_buffer_timer(State#state{buffer = [], last_log_id = OpId}),
+    % TODO: @gmcabrita should we reset the heartbeat timer here?
+    {noreply, State1}.
 
 handle_command({stable_time, Time}, _Sender, State) ->
     PingTxn = inter_dc_txn:ping(State#state.partition, State#state.last_log_id, Time),
-    {noreply, set_timer(broadcast(State, PingTxn))};
+    {noreply, set_heartbeat_timer(broadcast(State, PingTxn))};
 
 handle_command({hello}, _Sender, State) ->
-  {reply, ok, State};
+    {reply, ok, State};
 
 %% Handle the ping request, managed by the timer (1s by default)
 handle_command(ping, _Sender, State) ->
@@ -144,7 +168,8 @@ handle_exit(_Pid, _Reason, State) ->
 handoff_starting(_TargetNode, State) ->
     {true, State}.
 handoff_cancelled(State) ->
-    {ok, set_timer(State)}.
+     S1 = set_heartbeat_timer(State),
+    {ok, set_buffer_timer(S1)}.
 handoff_finished(_TargetNode, State) ->
     {ok, State}.
 handle_handoff_command( _Message , _Sender, State) ->
@@ -158,42 +183,76 @@ is_empty(State) ->
 delete(State) ->
     {ok, State}.
 terminate(_Reason, State) ->
-    _ = del_timer(State),
-  ok.
+    _ = del_heartbeat_timer(State),
+    _ = del_buffer_timer(State),
+    ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Cancels the ping timer, if one is set.
--spec del_timer(#state{}) -> #state{}.
-del_timer(State = #state{timer = none}) -> State;
-del_timer(State = #state{timer = Timer}) ->
-  _ = erlang:cancel_timer(Timer),
-  State#state{timer = none}.
+-spec del_heartbeat_timer(#state{}) -> #state{}.
+del_heartbeat_timer(State = #state{timer = none}) -> State;
+del_heartbeat_timer(State = #state{timer = Timer}) ->
+    _ = erlang:cancel_timer(Timer),
+    State#state{timer = none}.
 
 %% Cancels the previous ping timer and sets a new one.
--spec set_timer(#state{}) -> #state{}.
-set_timer(State) ->
-    set_timer(false,State).
+-spec set_heartbeat_timer(#state{}) -> #state{}.
+set_heartbeat_timer(State) ->
+    set_heartbeat_timer(false,State).
 
--spec set_timer(boolean(), #state{}) -> #state{}.
-set_timer(First, State = #state{partition = Partition}) ->
+-spec set_heartbeat_timer(boolean(), #state{}) -> #state{}.
+set_heartbeat_timer(First, State = #state{partition = Partition}) ->
     case First of
-	true ->
-	    {ok, Ring} = riak_core_ring_manager:get_my_ring(),
-	    Node = riak_core_ring:index_owner(Ring, Partition),
-	    MyNode = node(),
-	    case Node of
-		MyNode ->
-		    State1 = del_timer(State),
-		    State1#state{timer = riak_core_vnode:send_command_after(?HEARTBEAT_PERIOD, ping)};
-		_Other ->
-		    State
-	    end;
-	false ->
-	    State1 = del_timer(State),
-	    State1#state{timer = riak_core_vnode:send_command_after(?HEARTBEAT_PERIOD, ping)}
+        true ->
+            {ok, Ring} = riak_core_ring_manager:get_my_ring(),
+            Node = riak_core_ring:index_owner(Ring, Partition),
+            MyNode = node(),
+            case Node of
+                MyNode ->
+                    State1 = del_heartbeat_timer(State),
+                    State1#state{timer = riak_core_vnode:send_command_after(?HEARTBEAT_PERIOD, ping)};
+                _Other ->
+                    State
+            end;
+        false ->
+            State1 = del_heartbeat_timer(State),
+            State1#state{timer = riak_core_vnode:send_command_after(?HEARTBEAT_PERIOD, ping)}
     end.
 
+%% Cancels the txn buffer timer, if one is set.
+-spec del_buffer_timer(#state{}) -> #state{}.
+del_buffer_timer(State = #state{buffer_timer = none}) -> State;
+del_buffer_timer(State = #state{buffer_timer = Timer}) ->
+    _ = erlang:cancel_timer(Timer),
+    State#state{buffer_timer = none}.
+
+%% Cancels the previous txn buffer timer and sets a new one.
+-spec set_buffer_timer(#state{}) -> #state{}.
+set_buffer_timer(State) ->
+    case ?BUFFER_TXNS of
+        true -> set_buffer_timer(false,State);
+        false -> State
+    end.
+
+-spec set_buffer_timer(boolean(), #state{}) -> #state{}.
+set_buffer_timer(First, State = #state{partition = Partition}) ->
+    case First of
+        true ->
+            {ok, Ring} = riak_core_ring_manager:get_my_ring(),
+            Node = riak_core_ring:index_owner(Ring, Partition),
+            MyNode = node(),
+            case Node of
+                MyNode ->
+                    State1 = del_buffer_timer(State),
+                    State1#state{buffer_timer = riak_core_vnode:send_command_after(?BUFFER_TIMER, txn_send)};
+                _Other ->
+                    State
+            end;
+        false ->
+            State1 = del_buffer_timer(State),
+            State1#state{buffer_timer = riak_core_vnode:send_command_after(?BUFFER_TIMER, txn_send)}
+    end.
 
 %% Broadcasts the transaction via local publisher.
 -spec broadcast(#state{}, #interdc_txn{}) -> #state{}.
@@ -205,10 +264,9 @@ broadcast(State, Txn) ->
 %% Buffers the transaction so its operations can be compacted with operations in
 % other buffered transactions.
 -spec buffer(#state{}, #interdc_txn{}) -> #state{}.
-buffer(#state{partition = Partition} = State, Txn) ->
-  inter_dc_txn_buffer_vnode:buffer(Partition, Txn),
-  Id = inter_dc_txn:last_log_opid(Txn),
-  State#state{last_log_id_buffered = Id}.
+buffer(#state{txn_buffer = Buffer} = State, Txn) ->
+    Id = inter_dc_txn:last_log_opid(Txn),
+    State#state{last_log_id_buffered = Id, buffer = [Txn | Buffer]}.
 
 %% @doc Sends an async request to get the smallest snapshot time of active transactions.
 %%      No new updates with smaller timestamp will occur in future.
