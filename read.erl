22bfb18efd204d6d0eb7bf871c7ad503e974af22
Gonçalo Cabrita <_@gmcabrita.com>
2017-10-18 14:44:47 +0100

    .

--- read.erl
+++ read.erl
@@ -7,5 +7,5 @@
 main([Target]) ->
     erlang:set_cookie(node(), antidote),
     O = {obj, antidote_crdt_counter, bucket},
-    R = rpc:call(list_to_atom(Target), antidote, update_objects, [ignore, [], [{O, increment, 1}]]),
+    R = rpc:call(list_to_atom(Target), antidote, read_objects, [ignore, [], [O]]),
     io:format("~p~n", [R]).
--- txn2.erl
+++ txn2.erl
@@ -6,6 +6,7 @@
 
 main([Target]) ->
     erlang:set_cookie(node(), antidote),
-    O = {obj, antidote_crdt_counter, bucket},
-    R = rpc:call(list_to_atom(Target), antidote, update_objects, [ignore, [], [{O, increment, 1}]]),
+    O = {a_set, antidote_crdt_gset, bucket},
+    E = rand:uniform(100000),
+    R = rpc:call(list_to_atom(Target), antidote, update_objects, [ignore, [], [{O, add, E}]]),
     io:format("~p~n", [R]).
